8c39dcb553abf65cda1a9a9da41b97831bcfe842
Jerry Kurtin <gerald.kurtin7@gmail.com>
2023-01-28 23:46:37 -0600

    changes to forum

--- client/src/pages/forum/Forum.js
+++ client/src/pages/forum/Forum.js
@@ -8,18 +8,20 @@ const Forum = () => {
     const [window, setWindow] = useState("rec");
 
     return (
-        <div class="mainBody">
+        <>
             <div class="header">
                 <button class="titleButton returnButton">Return</button>
                 <button id="recButton" class={"titleButton recButton " + ((window === "rec") ? "selected" : "")} onClick={() => setWindow("rec")}>Recommendations</button>
                 <button id="discButton" class={"titleButton discButton " + ((window === "disc") ? "selected" : "")} onClick={() => setWindow("disc")}>Discussion</button>
             </div>
-            <div>{
-                (window === "rec") 
-                    ? <Recs></Recs>
-                    : <Discussion></Discussion>
-            }</div>
-        </div>
+            <div class="mainBody">
+                <div>{
+                    (window === "rec") 
+                        ? <Recs></Recs>
+                        : <Discussion></Discussion>
+                }</div>
+            </div>
+        </>
     )
 }
 
